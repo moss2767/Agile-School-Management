cd38bddaab55b72248e5941902e062a322fa4c73
moss <sashajmurray@pm.me>
2025-10-06 16:22:27 +0200

    Add a basic menurunner called in main

--- src/MenuRunner.java
+++ src/MenuRunner.java
@@ -1,7 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
 public class MenuRunner {
     Scanner scanner = new Scanner(System.in);
     static MenuRunner singleObject;
 
     public void run() {
+        InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
+            System.out.println("Welcome to school");
+            put("Show dummy student", () -> System.out.println("This is a student"));
+        }});
+    }
+
+    public static MenuRunner getInstance(){
+        if(singleObject == null){
+            singleObject = new MenuRunner();
+        }
+        return singleObject;
     }
 }
