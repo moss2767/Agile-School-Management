e73e5da4df701b9af17414573477d9c409218e11
Jonathan Rubensson <jonathan@eldfluga.se>
2025-10-07 14:28:21 +0200

    Implement SchoolSystem class with enrollStudentToClass.

---
+++ src/com/school/agileschool/common/SchoolSystem.java
@@ -0,0 +1,53 @@
+package com.school.agileschool.common;
+
+import com.school.agileschool.user.Person;
+import com.school.agileschool.user.Student;
+import com.school.agileschool.user.Teacher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolSystem {
+    private SchoolSystem instance;
+    private final Map<String, Course> coursesByID = new HashMap<>();
+    private final Map<String, Person> peopleByID = new HashMap<>();
+
+    private SchoolSystem(){}
+
+    public SchoolSystem getInstance() {
+        if (instance == null) {
+            instance = new SchoolSystem();
+        }
+        return instance;
+    }
+
+    public void addStudent(Student student){
+        peopleByID.put(student.getStudentID(), student);
+    }
+
+    public void addTeacher(Teacher teacher){
+        peopleByID.put(teacher.getTeacherID(), teacher);
+    }
+
+    public void addCourse(Course course){
+        coursesByID.put(course.getCourseID(), course);
+    }
+
+    public boolean enrollStudentToCourse(String studentID, String courseID){
+        if (!coursesByID.containsKey(courseID) || !peopleByID.containsKey(studentID)){
+            return false;
+        }
+        Course course = coursesByID.get(courseID);
+        if (course.getEnrolledStudentsByID().contains(studentID)){
+            return false;
+        }
+        if (peopleByID.get(studentID) instanceof Student student) {
+            if (student.getCourses().contains(courseID)) {
+                return false;
+            }
+            course.enrollStudentByID(studentID);
+            student.addCourse(courseID);
+        }
+        return false;
+    }
+}
