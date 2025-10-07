0832539cfcbd3d54fefd439630ddfac3b37b3128
robine81 <robine.westberg@gmail.com>
2025-10-07 16:20:43 +0200

    Created an ID generator for Person inherited by Student and Teacher

--- src/com/school/agileschool/user/Person.java
+++ src/com/school/agileschool/user/Person.java
@@ -1,24 +1,34 @@
 package com.school.agileschool.user;
 
 public abstract class Person {
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
 
     public Person(){
 
     }
 
-    public Person (String name, String email) {
-        this.name = name;
+    public Person (String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
     }
 
-    public String getName () {
-        return name;
+    public String getFirstName () {
+        return firstName;
     }
 
-    public void setName (String name) {
-        this.name = name;
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail () {
@@ -27,5 +37,9 @@ public abstract class Person {
 
     public void setEmail (String email) {
         this.email = email;
+    }
+
+    public String generatePersonID(){
+        return String.format("%s%s%d", this.firstName.substring(0, 3).toUpperCase(), this.lastName.substring(0,3).toUpperCase(), (int)(Math.random()*100));
     }
 }
