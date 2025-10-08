8dde6616c7d25573e06b4dcea6686b514e01a7ad
moss <sashajmurray@pm.me>
2025-10-08 17:09:22 +0200

    manual conflict fix

--- src/com/school/agileschool/common/Course.java
+++ src/com/school/agileschool/common/Course.java
@@ -44,14 +44,4 @@ public class Course {
     public void unenrollStudentByID(String studentID){
         enrolledStudentsByID.remove(studentID);
     }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("Course ID: %s%n", this.getCourseID()));
-        sb.append(String.format("Course Name: %s%n", this.getName()));
-        sb.append(String.format("Assigned Teacher ID: %s%n", this.getAssignedTeacherID()));
-        sb.append(String.format("Enrolled students by ID: %s%n", this.getEnrolledStudentsByID()));
-        return sb.toString();
-    }
 }
