1452254bfc8496db3266fc1be41ccfb3ad0d0086
Tomas <mac@Macs-MacBook-Air.local>
2025-10-08 15:18:45 +0200

    Basic methods for user management in the User MenuRunner class.

--- src/com/school/agileschool/menu/MenuRunnerUser.java
+++ src/com/school/agileschool/menu/MenuRunnerUser.java
@@ -1,48 +1,68 @@
 package com.school.agileschool.menu;
 
 import com.school.agileschool.persistence.JSONDB;
+import com.school.agileschool.user.Person;
 import com.school.agileschool.user.Student;
 import com.school.agileschool.utilities.InputManagementHandler;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MenuRunnerUser {
+    private static String studentFullName(Student student) {
+        return student.getFirstName() + " " + student.getLastName();
+    }
+    public static void runStudentAdministration(Student student){
+
+    }
+    public static void selectAndRunStudentAdministration(List<Student> students){
+        Map<String, Callable<Student>> menu = students.stream()
+                .collect(
+                        Collectors.toMap(
+                                Person::getName,
+                                student -> () -> student,
+                                (existing, replacement) -> existing, // merge function in case of duplicate keys
+                                HashMap::new       // supplier for the HashMap
+                        )
+                );
+        runStudentAdministration(InputManagementHandler.runMenuType(menu));
+    }
     public static void run() {
         InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
             put("Enroll a student to the school", () -> {
-                String firstName = InputManagementHandler.getLineAsString("First name");
-                String lastName = InputManagementHandler.getLineAsString("Last name");
-                String email = InputManagementHandler.getLineAsString("Email");
+                List<String> keys = Arrays.asList("First name", "Last name", "Email");
+                Map<String, String> userInputAsHashMap = InputManagementHandler.fillHashMapWithScan(keys);
+                Student student = new Student(userInputAsHashMap.get("First name"), userInputAsHashMap.get("Last name"), userInputAsHashMap.get("Email"));student.modifyStudentID(userInputAsHashMap.get("ID"));
+                student.modifyStudentID(student.generatePersonID());
+                JSONDB.getInstance()
+                        .addStudent(student.getStudentID(), student);
             });
             put("Administer an existing student", () -> {
-                String searchInput = InputManagementHandler.getLineAsString("Search");
-                Pattern matchingPattern = Pattern.compile(searchInput, Pattern.CASE_INSENSITIVE);
-                List<Student> matches = JSONDB.getInstance()
-                        .getStudents()
-                        .stream()
-                        .filter(s -> matchingPattern.matcher(s.getName()).find())
-                        .collect(Collectors.toUnmodifiableList());
-                if (matches.isEmpty()) {
-                    System.out.println("No matches found");
-                }
-                Map<String, Callable<Student>> menu = matches.stream()
-                        .collect(
-                                Collectors.toMap(
-                                        Student::getName,
-                                        student -> () -> student,
-                                        (existing, replacement) -> existing, // merge function in case of duplicate keys
-                                        HashMap::new       // supplier for the HashMap
-                                )
-                        );
-                Student selectStudent = InputManagementHandler.runMenuType(menu);
-                System.out.println(selectStudent.getName());
+                InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
+                    put("Select", () -> {
+                        selectAndRunStudentAdministration(JSONDB.getInstance().getStudents());
+                    });
+                    put("Filter and select", () -> {
+                        String searchInput = InputManagementHandler.getLineAsString("Search");
+                        Pattern matchingPattern = Pattern.compile(searchInput, Pattern.CASE_INSENSITIVE);
+                        List<Student> matches = JSONDB.getInstance()
+                                .getStudents()
+                                .stream()
+                                .filter(s -> matchingPattern.matcher(studentFullName(s)).find())
+                                .collect(Collectors.toUnmodifiableList());
+                        if (matches.isEmpty()) {
+                            System.out.println("No matches found");
+                        }
+                        else {
+                            selectAndRunStudentAdministration(matches);
+                        }
+
+                    });
+                }});
+
             });
         }});
     }
