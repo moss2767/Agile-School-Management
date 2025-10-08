4851162550c5c1ce2a3fdc49489bc0e1d4621cc3
Tomas <mac@Macs-MacBook-Air.local>
2025-10-08 15:43:33 +0200

    First skeleton of MenuRunnerUser

--- src/com/school/agileschool/menu/MenuRunnerUser.java
+++ src/com/school/agileschool/menu/MenuRunnerUser.java
@@ -1,5 +1,6 @@
 package com.school.agileschool.menu;
 
+import com.school.agileschool.common.SchoolSystem;
 import com.school.agileschool.persistence.JSONDB;
 import com.school.agileschool.user.Person;
 import com.school.agileschool.user.Student;
@@ -16,9 +17,15 @@ public class MenuRunnerUser {
         return student.getFirstName() + " " + student.getLastName();
     }
     public static void runStudentAdministration(Student student){
+        System.out.println("You are now administering student: " + student.getName());
+        InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
+            put("Remove the student from the system", () -> {
 
+            });
+        }});
     }
     public static void selectAndRunStudentAdministration(List<Student> students){
+        System.out.println("Select a student");
         Map<String, Callable<Student>> menu = students.stream()
                 .collect(
                         Collectors.toMap(
@@ -42,10 +49,10 @@ public class MenuRunnerUser {
             });
             put("Administer an existing student", () -> {
                 InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
-                    put("Select", () -> {
+                    put("Select from all students", () -> {
                         selectAndRunStudentAdministration(JSONDB.getInstance().getStudents());
                     });
-                    put("Filter and select", () -> {
+                    put("Filter and select students", () -> {
                         String searchInput = InputManagementHandler.getLineAsString("Search");
                         Pattern matchingPattern = Pattern.compile(searchInput, Pattern.CASE_INSENSITIVE);
                         List<Student> matches = JSONDB.getInstance()
@@ -56,10 +63,12 @@ public class MenuRunnerUser {
                         if (matches.isEmpty()) {
                             System.out.println("No matches found");
                         }
+                        else if (matches.size() == 1) {
+                            runStudentAdministration(matches.get(0));
+                        }
                         else {
                             selectAndRunStudentAdministration(matches);
                         }
-
                     });
                 }});
 
