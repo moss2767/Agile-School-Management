c96dc7f2200339fec7f3b68f68dab1bb1cc4b18f
moss <sashajmurray@pm.me>
2025-10-08 14:13:10 +0200

    use main menu runner, add submenus

--- src/com/school/agileschool/menu/MenuRunner.java
+++ src/com/school/agileschool/menu/MenuRunner.java
@@ -11,11 +11,8 @@ public class MenuRunner {
     public void run() {
         InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
             System.out.println("Welcome to school");
-            put("Show dummy student", () -> System.out.println("This is a student"));
-            put("Submenu", () -> InputManagementHandler.runMenuUntilQuit(new LinkedHashMap<>() {{
-                put("Option 1", ()-> System.out.println("Option 1"));
-                put("Option 2", ()-> System.out.println("Option 2"));
-            }}));
+            put("User Menu", MenuRunnerUser::run);
+            put("Course Menu", CourseMenu::run);
         }});
     }
 
