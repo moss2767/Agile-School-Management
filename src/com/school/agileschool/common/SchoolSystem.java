326a2025b9fdbf7d4d15c6f85ebef51416880c8a
Jonathan Rubensson <jonathan@eldfluga.se>
2025-10-08 15:53:45 +0200

    Clean class after rebase.

--- src/com/school/agileschool/common/SchoolSystem.java
+++ src/com/school/agileschool/common/SchoolSystem.java
@@ -1,34 +1,16 @@
 package com.school.agileschool.common;
 
 import com.school.agileschool.persistence.JSONDB;
-import com.school.agileschool.user.Person;
 import com.school.agileschool.user.Student;
-import com.school.agileschool.user.Teacher;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SchoolSystem {
     private SchoolSystem instance;
-    private final Map<String, Course> coursesByID = new HashMap<>();
-    private final Map<String, Person> peopleByID = new HashMap<>();
 
     private SchoolSystem(){}
-
-    public void addStudent(Student student){
-        peopleByID.put(student.getStudentID(), student);
-    }
-
-    public void addTeacher(Teacher teacher){
-        peopleByID.put(teacher.getTeacherID(), teacher);
-    }
-
-    public void addCourse(Course course){
-        coursesByID.put(course.getCourseID(), course);
-    }
 
     public Optional<List<Student>> getStudentsEnrolledInCourse(String courseId) {
         Optional<Course> course = JSONDB.getInstance().getCourseById(courseId);
