be1e3ae996ea46742bd8425bbc63dab8ebbdcdad
Jonathan Rubensson <jonathan@eldfluga.se>
2025-10-09 15:42:18 +0200

    Refactor Grade to enum.

--- src/com/school/agileschool/common/Grade.java
+++ src/com/school/agileschool/common/Grade.java
@@ -1,32 +1,5 @@
 package com.school.agileschool.common;
 
-public class Grade {
-    public enum Mark {
-        F, E, D, C, B, A
-    }
-
-    private final String courseID;
-    private Mark mark;
-
-    public Grade(Mark mark, String courseID){
-        this.mark = mark;
-        this.courseID = courseID;
-    }
-
-    public String getCourseID(){
-        return courseID;
-    }
-
-    public Mark getMark(){
-        return mark;
-    }
-
-    public String getMarkAsString(){
-        return mark.toString();
-    }
-
-    public int getMarkAsInt(){
-        return mark.ordinal();
-    }
-
+public enum Grade {
+    F, E, D, C, B, A
 }
