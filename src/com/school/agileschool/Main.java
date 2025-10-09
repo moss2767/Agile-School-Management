215b76bc6b4edbe43601c9a0c075c5e7470a17a2
Tomas <mac@Macs-MacBook-Air.local>
2025-10-09 13:56:44 +0200

    Adding save functionality

--- src/com/school/agileschool/Main.java
+++ src/com/school/agileschool/Main.java
@@ -4,13 +4,19 @@ import com.school.agileschool.menu.MenuRunner;
 import com.school.agileschool.persistence.JSONDB;
 
 public class Main {
+    private static JSONDB db;
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         try {
-            JSONDB.getInstanceFromDisk();
+            db = JSONDB.getInstanceFromDisk();
         } catch(Exception e) {
             throw new RuntimeException("Runtime error", e);
         }
         MenuRunner.getInstance().run();
+        try {
+            db.writeToDisk();
+        } catch(Exception e) {
+            throw new RuntimeException("Runtime error", e);
+        }
     }
 }
